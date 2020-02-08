2cca3413349ab82df651daa173c0007e176fb9bc
venustuspl <venustus.pl@gmail.com>
2020-02-08 09:04:00 +0100

    0.0.1

---
+++ src/main/java/pl/venustus/H2Api/H2ApiApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.H2Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/H2Api/H2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.H2Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
